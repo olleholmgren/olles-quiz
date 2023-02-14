1c8d74447bbf2422a684c9f11f5fbef11803f2b6
olleholmgren <olle.holmgren@gmail.com>
2023-02-14 17:25:15 +0000

    add test questions in js and test code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-function startGame() {
+function startPage() {
     alert("Welcome to this quiz")
 }
+startPage()
 
-startGame()
+let city = "";
+
+function startQuiz(city) {
+       let question = document.getElementById("question");
+       question.innerText = question["city"].q:
+        
+}
+
+startQuiz()
+
+
+
+/**
+ * Questions for the quiz
+ */
+let question = [{
+    city: 0,
+q: "Madrid",
+a: [{ text: "Belgium", isCorrect: false },
+    { text: "Sweden", isCorrect: false },
+    { text: "Spain", isCorrect: true },
+    { text: "Greece", isCorrect: false }
+]
+    city: 1,
+q: "Sofia"
+a: [{ text: "Bulgaria", isCorrect: true },
+    { text: "Bosnia and Herzegovina", isCorrect: false },
+    { text: "Germany", isCorrect: false },
+    { text: "Iceland", isCorrect: false }
+]
+}
+]
